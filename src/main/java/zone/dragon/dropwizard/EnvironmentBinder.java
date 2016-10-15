b21cba23a12e841893027c027cc55a6dcd92d6bb
Darth Android <darthandroid@gmail.com>
2016-10-15 18:29:11 -0500

    Leverage helper method

--- src/main/java/zone/dragon/dropwizard/EnvironmentBinder.java
+++ src/main/java/zone/dragon/dropwizard/EnvironmentBinder.java
@@ -2,19 +2,19 @@ package zone.dragon.dropwizard;
 
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.health.HealthCheckRegistry;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.Configuration;
 import io.dropwizard.lifecycle.setup.LifecycleEnvironment;
 import io.dropwizard.setup.Environment;
 import lombok.NonNull;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.util.component.AbstractLifeCycle.AbstractLifeCycleListener;
-import org.eclipse.jetty.util.component.LifeCycle;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
 /**
  * This binder makes much of the DropWizard environment available to HK2 to be injected into components that request it at runtime.
  * Specifically, the following components are bound: <ul> <li>{@link Environment}</li> <li>{@link HealthCheckRegistry}</li> <li>{@link
- * LifecycleEnvironment}</li> <li>{@link MetricRegistry}</li> <li>{@link Configuration}</li> </ul>
+ * LifecycleEnvironment}</li> <li>{@link MetricRegistry}</li> <li>{@link Configuration}</li>
+ * <li>{@link ObjectMapper}</li> <li>{@link Server}</li></ul>
  */
 public class EnvironmentBinder<T> extends AbstractBinder {
     private final T           configuration;
@@ -32,14 +32,7 @@ public class EnvironmentBinder<T> extends AbstractBinder {
     public EnvironmentBinder(@NonNull T configuration, @NonNull Environment environment) {
         this.configuration = configuration;
         this.environment = environment;
-        environment.lifecycle().addLifeCycleListener(new AbstractLifeCycleListener() {
-            @Override
-            public void lifeCycleStarting(LifeCycle event) {
-                if (event instanceof Server) {
-                    server = (Server) event;
-                }
-            }
-        });
+        environment.lifecycle().addServerLifecycleListener(server1 -> server = server1);
     }
 
     @SuppressWarnings("unchecked")
@@ -51,5 +44,6 @@ public class EnvironmentBinder<T> extends AbstractBinder {
         bind(environment.metrics()).to(MetricRegistry.class);
         bind(configuration).to((Class) configuration.getClass()).to(Configuration.class);
         bind(server).to(Server.class);
+        bind(environment.getObjectMapper()).to(ObjectMapper.class);
     }
 }
